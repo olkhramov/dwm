fa8e688789b6d7bb4d8ee6d36dd4c7e3a32d8e03
Oleksii Khramov <118835793+olkhramov@users.noreply.github.com>
2026-08-29 18:32:01 +0200

    overhaul config.h aesthetics (Nord theme, JetBrainsMono Nerd Font, gaps)

--- config.h
+++ config.h
@@ -6,26 +6,26 @@
 #define BROWSER "librewolf"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 8;        /* horiz inner gap between windows */
+static unsigned int gappiv    = 8;        /* vert inner gap between windows */
+static unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static int vertpad            = 8;        /* vertical padding of bar */
 static int sidepad            = 8;        /* horizontal padding of bar */
-static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
-static char urgbordercolor[]        = "#ff0000";
+static char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "monospace:size=11", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
+static char normbgcolor[]           = "#2e3440";
+static char normbordercolor[]       = "#3b4252";
+static char normfgcolor[]           = "#d8dee9";
+static char selfgcolor[]            = "#eceff4";
+static char selbordercolor[]        = "#88c0d0";
+static char selbgcolor[]            = "#434c5e";
+static char urgbordercolor[]        = "#bf616a";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
